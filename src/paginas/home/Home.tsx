f3aed85fe52b50b1d068524e1a27955129e1b766
Gabriel <gabrielssantiago095@hotmail.com>
2023-03-01 00:52:31 -0300

    criando página home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="imagem da tela inicial" className="img"/>
+        </>
+    )
+}
+
+
+export default Home;
